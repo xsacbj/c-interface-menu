8b7025a226a6ca3d2b2945cff1db848c6bc0b9ad
Sérgio Augusto <sacbj@icomp.ufam.edu.br>
2020-03-14 14:48:51 -0400

    arquivo main com exemplos de utilização da biblioteca de Menu

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "getch/getch.h"
 #include "menu/menu.h"
 
+
+Menu menu;
+
+void hello(void){
+  printf("Hello World");
+  getch();
+}
+
+void exiting(void){
+  exitMenu(&menu);
+}
+
 int main(){
+
+  createMenu(&menu);
+  
+  createOptionMenu("Print Hello World",&hello, &menu);
+  createOptionMenu("Print Hello World",&hello, &menu);
+  createOptionMenu("Print Hello World",&hello, &menu);
+  createOptionMenu("Exit Menu", &exiting, &menu);
+
+  menuLoop(&menu);
 
   return 0;
 }
